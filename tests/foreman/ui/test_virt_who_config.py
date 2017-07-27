dd185de19270acd0c9fc2654b456e7bb3261eaee
Perry Gagne <pgagne@redhat.com>
2017-07-27 15:22:19 -0400

    updated stubs

--- tests/foreman/ui/test_virt_who_config.py
+++ tests/foreman/ui/test_virt_who_config.py
@@ -14,7 +14,7 @@
 """
 
 from robottelo.decorators import run_only_on, stubbed, tier1, tier2, tier3
-from robottelo.test import UITestCase, TestCase
+from robottelo.test import UITestCase, TestCase, APITestCase
 
 
 class VirtWhoConfigUITestCase(UITestCase):
@@ -23,7 +23,7 @@ class VirtWhoConfigUITestCase(UITestCase):
     @run_only_on('sat')
     @stubbed()
     @tier1
-    def test_postive_config_page_populated(self):
+    def test_positive_config_page_populated(self):
         """DESCRIPTION
 
         :id: db6bbc68-2047-4c7d-af5b-31aee0030318
@@ -41,27 +41,122 @@ class VirtWhoConfigUITestCase(UITestCase):
         :CaseImportance: Critical
         """
 
-    def test_postive_config_page_empty(self):
+    def test_positive_config_page_empty(self):
         """
         2
          
         """
         pass
 
-    def test_postive_dashboard_no_reports(self):
+    def test_positive_dashboard_no_reports(self):
         pass
 
-    def test_postive_dashboard_out_of_date(self):
+    def test_positive_dashboard_out_of_date(self):
         pass
 
-    def test_postive_dashboard_up_to_date(self):
+    def test_positive_dashboard_up_to_date(self):
         pass
 
-    def test_postive_lastest_out_of_date(self):
+    def test_positive_lastest_out_of_date(self):
         pass
 
-    def test_postive_vm_create(self):
+    def test_positive_vm_create(self):
         pass
+
+    def test_negative_vm_create(self):
+        pass
+
+    def test_positive_config_update(self):
+        pass
+
+    def test_negative_config_update(self):
+        pass
+
+
+class VirtWhoConfigRoleApiTests(APITestCase):
+    
+    def test_positive_role_manager(self):
+        """virt-who Manager
+        a. Create a user with ONLY the virt-who manager role.
+        b. Verify the user can create virt-who configurations
+        c. Verify the user can edit an existing virt-who configuration
+        d. Verify the user can delete a virt-who configuration
+        e. Verify the user can see virt-who reporting information through the dashboard
+        f. Verify the user can do no other actions
+        :return:
+        """
+        pass
+    
+    def test_negative_role_manager(self):
+        """
+        f. Verify the user can do no other actions
+
+        :return:
+        """
+        pass
+
+    def test_positive_role_reporter(self):
+        """
+        2. virt-who Reporter
+        a. Create a user with ONLY the virt-who reporter role.
+        b. Configure virt-who WITHOUT using the virt-who config plugin. Set the Satellite the created user.
+        c. Create a vm to cause virt-who to send a report to satellite.
+        d. Verify the virt-who server send a report to the satellite.
+        """
+        pass
+
+    def test_negative_role_reporter(self):
+        """
+                e. Verify the user can do no other actions
+
+        :return:
+        """
+        pass
+
+    def test_positive_role_viewer(self):
+        """
+        3. virt-who Viewer
+        a. Create a user with ONLY the virt-who Viewer role.
+        b. Verify the user can view virt-who configurations.
+        c. Verify the user CANNOT delete or modify virt-who configurations
+        """
+        pass
+
+    def test_negative_role_viewer(self):
+        """
+        d. Verify the user can do no other actions
+
+        :return:
+        """
+        pass
+
+
+
+class VirtWhoConfigUpgrade(UITestCase):
+
+    def test_positive_satellite_upgrade(self):
+        """ Satellite upgrade
+
+        :uuid: 0e301c08-8bef-4bea-a690-d4b0760949e8
+
+        :Steps:
+
+        1. Start with a satellite version before the virt-who configurator plugin
+        2. Configure a virt-who server with virtualization provider (VP1) to send reports to the satellite.
+        3. Upgrade the satellite to a version with virt-who configurator plugin.
+        4. Configure a new virtualization provider (VP2)  with a VDC subscription
+        5. Using the virt-who plugin, create a configuration for the new hypervisor.
+        6. Deploy the configuration to the virt-who server
+        7. Create a guest on VP1
+        8. Create a guest on VP2
+        9. verify that that reports on VP1 and VP2 are correct.
+        """
+
+
+
+
+
+
 
 
 class VirtWhoConfigCLITestCase(UITestCase):
